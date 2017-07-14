cc21538d53c2fe936c23ff7182b29eea3cad2cfa
Marcello Albuquerque <marcelloma@users.noreply.github.com>
2017-07-14 14:28:46 -0300

    Improve etag middleware (#14)

--- etag/middleware.go
+++ etag/middleware.go
@@ -2,8 +2,10 @@ package etag
 
 import (
 	"crypto/md5"
+	"errors"
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 const (
@@ -14,31 +16,63 @@ const (
 	IfNoneMatchHeaderKey = "If-None-Match"
 )
 
+var (
+	errDoubleWrite = errors.New("write called twice with ETag Middleware")
+)
+
 type etagResponseWriter struct {
 	http.ResponseWriter
-	req  *http.Request
-	code int
+	req         *http.Request
+	code        int
+	wroteHeader bool
+	wroteBody   bool
+	headerLock  sync.Mutex
+	bodyLock    sync.Mutex
 }
 
 func (erw *etagResponseWriter) Write(b []byte) (int, error) {
-	etag := etag(b)
-	erw.Header().Set(ETagHeaderKey, etag)
-	if erw.req.Header.Get(IfNoneMatchHeaderKey) == etag {
-		erw.WriteHeader(http.StatusNotModified)
-		return erw.Write(nil)
+	etag := calculateEtag(b)
+	if erw.code < http.StatusMultipleChoices {
+		erw.Header().Set(ETagHeaderKey, etag)
 	}
 
-	erw.WriteHeader(erw.code)
-	return erw.ResponseWriter.Write(b)
+	if erw.req.Header.Get(IfNoneMatchHeaderKey) == etag && erw.req.Method == http.MethodGet {
+		erw.code = http.StatusNotModified
+		erw.writeHeader()
+		return erw.writeBody(nil)
+	}
+
+	erw.writeHeader()
+	return erw.writeBody(b)
 }
 
-func etag(data []byte) string {
+func calculateEtag(data []byte) string {
 	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func (erw *etagResponseWriter) writeBody(data []byte) (int, error) {
+	erw.bodyLock.Lock()
+	defer erw.bodyLock.Unlock()
+
+	if erw.wroteBody {
+		panic(errDoubleWrite)
+	}
+	erw.wroteBody = true
+	return erw.ResponseWriter.Write(data)
+}
+
+func (erw *etagResponseWriter) writeHeader() {
+	erw.headerLock.Lock()
+	defer erw.headerLock.Unlock()
+
+	if !erw.wroteHeader {
+		erw.wroteHeader = true
+		erw.ResponseWriter.WriteHeader(erw.code)
+	}
 }
 
 func (erw *etagResponseWriter) WriteHeader(code int) {
 	erw.code = code
-	erw.ResponseWriter.WriteHeader(code)
 }
 
 // Middleware -
@@ -54,5 +88,7 @@ func NewMiddleware() Middleware {
 }
 
 func (mw *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	next(&etagResponseWriter{w, r, http.StatusOK}, r)
+	writer := &etagResponseWriter{w, r, http.StatusOK, false, false, sync.Mutex{}, sync.Mutex{}}
+	next(writer, r)
+	writer.writeHeader()
 }
--- etag/middleware_test.go
+++ etag/middleware_test.go
@@ -25,7 +25,7 @@ func TestResponseWithEtag(t *testing.T) {
 
 	srv.ServeHTTP(response, req)
 
-	exptectedEtag := etag([]byte(data))
+	exptectedEtag := calculateEtag([]byte(data))
 
 	if response.Header().Get(ETagHeaderKey) != exptectedEtag {
 		t.Errorf("Unexpected ETag. Want: %v, got: %v", exptectedEtag, response.Header().Get(ETagHeaderKey))
@@ -37,7 +37,7 @@ func TestResponseNotModified(t *testing.T) {
 	response := httptest.NewRecorder()
 
 	req := httptest.NewRequest("GET", "/", nil)
-	req.Header.Add(IfNoneMatchHeaderKey, etag([]byte(data)))
+	req.Header.Add(IfNoneMatchHeaderKey, calculateEtag([]byte(data)))
 
 	srv.UseFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		w.Write([]byte(data))
@@ -52,5 +52,70 @@ func TestResponseNotModified(t *testing.T) {
 
 	if len(response.Body.Bytes()) > 0 {
 		t.Errorf("Unexpected Body. Want to be nil, got: %v", response.Body.Bytes())
+	}
+}
+
+func TestResponseNotModifiedOnlyOnGet(t *testing.T) {
+	for _, method := range []string{"POST", "DELETE", "PUT", "PATCH", "HEAD"} {
+		srv := negroni.New(NewMiddleware())
+		response := httptest.NewRecorder()
+
+		req := httptest.NewRequest(method, "/", nil)
+
+		srv.UseFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+			w.Write([]byte(data))
+			next(w, r)
+		})
+
+		srv.ServeHTTP(response, req)
+
+		if response.Code != http.StatusOK {
+			t.Errorf("Unexpected Status Code. Want: %v, got: %v", http.StatusOK, response.Code)
+		}
+	}
+}
+
+func TestPanicsOnSecondWrite(t *testing.T) {
+	var thrownError error
+	srv := negroni.New(NewMiddleware())
+	response := httptest.NewRecorder()
+
+	req := httptest.NewRequest("GET", "/", nil)
+
+	srv.UseFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+		w.Write([]byte(data))
+		defer func() {
+			if r := recover(); r != nil {
+				thrownError = r.(error)
+			}
+		}()
+		w.Write([]byte(data))
+		next(w, r)
+	})
+
+	srv.ServeHTTP(response, req)
+
+	if thrownError != errDoubleWrite {
+		t.Errorf("Expected double write to panic with error %v, got: %v", errDoubleWrite, thrownError)
+	}
+}
+
+func TestDoesNotSetEtagWhenStatusCodeGt300(t *testing.T) {
+	srv := negroni.New(NewMiddleware())
+	response := httptest.NewRecorder()
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	srv.UseFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(data))
+		next(w, r)
+	})
+
+	srv.ServeHTTP(response, req)
+
+	if etagHeader := response.Header().Get(ETagHeaderKey); etagHeader != "" {
+		t.Errorf("Expected empty etag header, got: %v", etagHeader)
 	}
 }
