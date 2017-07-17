7dc127fefa38bf3de2d3144fba7a9339eab393ed
Rodrigo Dumont <rlsdumont@gmail.com>
2017-07-17 17:21:06 -0300

    Add examples

---
+++ events/example_publisher_test.go
@@ -0,0 +1,22 @@
+package events_test
+
+import (
+	"fmt"
+
+	"github.com/hellomd/go-sdk/events"
+)
+
+func ExamplePublisher() {
+	pub, err := events.NewPublisher("amqp://guest:guest@localhost")
+	if err != nil {
+		panic(fmt.Errorf("there was a problem asserting AMQP structure: %v", err))
+	}
+
+	err = pub.Publish("questions.article.created", map[string]interface{}{
+		"id":    "abc123",
+		"title": "What is the meaning of life?",
+	})
+	if err != nil {
+		panic(fmt.Errorf("error publishing question: %v", err))
+	}
+}
