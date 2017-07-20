b21e31f10320d5f8db3bd0631793aa076d2b1359
Marcello Albuquerque <marcello.m.albuquerque@gmail.com>
2017-07-20 19:37:38 -0300

    Improve ctxKey

--- authentication/context.go
+++ authentication/context.go
@@ -5,15 +5,13 @@ import (
 	"errors"
 )
 
-type ctxKeyType int
-
-const ctxKey ctxKeyType = 0
+type ctxKey struct{}
 
 var errNoUserInCtx = errors.New("no user in context")
 
 // GetUserFromCtx -
 func GetUserFromCtx(ctx context.Context) *User {
-	u, ok := ctx.Value(ctxKey).(*User)
+	u, ok := ctx.Value(ctxKey{}).(*User)
 	if !ok {
 		panic(errNoUserInCtx)
 	}
@@ -22,5 +20,5 @@ func GetUserFromCtx(ctx context.Context) *User {
 
 // SetUserInCtx -
 func SetUserInCtx(ctx context.Context, user *User) context.Context {
-	return context.WithValue(ctx, ctxKey, user)
+	return context.WithValue(ctx, ctxKey{}, user)
 }
