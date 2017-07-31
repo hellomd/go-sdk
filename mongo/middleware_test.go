5ea69554924bf9f21b2236492ac8d9b6fa5e90bd
Gabriel Arrais <arraiscgabriel@gmail.com>
2017-07-31 11:14:40 -0300

    implements mongo url parsing to enable ssl connections

--- mongo/middleware_test.go
+++ mongo/middleware_test.go
@@ -3,27 +3,47 @@ package mongo
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/hellomd/go-sdk/config"
 	"github.com/urfave/negroni"
 )
 
-func TestMiddleware(t *testing.T) {
-	dbName := "test"
-	mw := NewMiddleware(config.Get(URLCfgKey), dbName).(*middleware)
+func TestMiddlewareWithoutSSL(t *testing.T) {
+	mdw, err := NewMiddleware(config.Get(URLCfgKey)+"/test", false)
+	if err != nil {
+		t.Error("Could not create Middleware: " + err.Error())
+	}
+
+	test(mdw, t)
+}
+
+func TestMiddlewareWithSSL(t *testing.T) {
+	mdw, err := NewMiddleware(config.Get(URLCfgKey)+"/test?ssl=true", true)
+	if err != nil {
+		t.Error("Could not create Middleware: " + err.Error())
+	}
+
+	test(mdw, t)
+}
+
+func test(mdw Middleware, t *testing.T) {
+	mw := mdw.(*middleware)
 	req := httptest.NewRequest("GET", "/", nil)
 	response := httptest.NewRecorder()
 
 	a := negroni.New()
 	a.Use(mw)
+
 	a.Use(negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		dbName := strings.TrimPrefix(mw.mongoURL.Path, "/")
 		v, err := GetFromCtx(r.Context())
 		if err == errNotInCtx {
 			t.Error("Expected database on context")
 		}
 		if v.Name != dbName {
-			t.Errorf("Expected %s database name, got: %s", dbName, v.Name)
+			t.Errorf("Expected %s database name, got: %s", v.Name, dbName)
 		}
 		if v.Session == mw.session {
 			t.Errorf("Expected session to be different from stored session")
