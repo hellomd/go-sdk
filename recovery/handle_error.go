283933afcc8e31c96adcb0b4d186b00b5ad2609b
Rodrigo Dumont <rlsdumont@gmail.com>
2017-08-08 10:37:00 -0300

    Always log recovered stack traces (#30)
    * Always log recovered stack traces
    
    * Expose alterantive error handler for debug

--- recovery/handle_error.go
+++ recovery/handle_error.go
@@ -2,6 +2,8 @@ package recovery
 
 import (
 	"context"
+	"fmt"
+	"runtime/debug"
 
 	"github.com/hellomd/go-sdk/logger"
 	"github.com/hellomd/go-sdk/recovery/sentry"
@@ -12,6 +14,19 @@ func HandleError(ctx context.Context, err error) {
 	logger, ctxErr := logger.GetFromCtx(ctx)
 	if ctxErr == nil {
 		logger.Error(err)
+	}
+
+	sentry, ctxErr := sentry.GetFromCtx(ctx)
+	if ctxErr == nil {
+		sentry.CaptureError(err, nil)
+	}
+}
+
+// DebugHandleError reports given error to logger including stacktrace and sentry when they are available
+func DebugHandleError(ctx context.Context, err error) {
+	logger, ctxErr := logger.GetFromCtx(ctx)
+	if ctxErr == nil {
+		logger.WithField("stack_trace", fmt.Sprintf("%s", debug.Stack())).Error(err)
 	}
 
 	sentry, ctxErr := sentry.GetFromCtx(ctx)
