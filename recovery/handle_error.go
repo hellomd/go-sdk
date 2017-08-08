374910020004909dc440f05a0df232e777b230ee
Rodrigo Dumont <rlsdumont@gmail.com>
2017-08-08 10:35:44 -0300

    Expose alterantive error handler for debug

--- recovery/handle_error.go
+++ recovery/handle_error.go
@@ -13,6 +13,19 @@ import (
 func HandleError(ctx context.Context, err error) {
 	logger, ctxErr := logger.GetFromCtx(ctx)
 	if ctxErr == nil {
+		logger.Error(err)
+	}
+
+	sentry, ctxErr := sentry.GetFromCtx(ctx)
+	if ctxErr == nil {
+		sentry.CaptureError(err, nil)
+	}
+}
+
+// DebugHandleError reports given error to logger including stacktrace and sentry when they are available
+func DebugHandleError(ctx context.Context, err error) {
+	logger, ctxErr := logger.GetFromCtx(ctx)
+	if ctxErr == nil {
 		logger.WithField("stack_trace", fmt.Sprintf("%s", debug.Stack())).Error(err)
 	}
 
