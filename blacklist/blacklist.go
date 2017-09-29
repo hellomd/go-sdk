e6f9b4832728b6c3fc40664b0693f7e1c738bc52
Marcello Albuquerque <marcello.m.albuquerque@gmail.com>
2017-09-29 08:31:11 -0300

    Add blacklist middleware

---
+++ blacklist/blacklist.go
@@ -0,0 +1,14 @@
+package blacklist
+
+var healthcheck = "/healthcheck"
+
+var blacklist = []string{
+	"/phpmyadmin2018",
+	"/phpmyadmin2017",
+	"/phpmyadmin2016",
+	"/phpmyadmin2015",
+	"/phpmyadmin2014",
+	"/phpmyadmin2013",
+	"/phpmyadmin2012",
+	"/phpmyadmin2011",
+}
---
+++ blacklist/middleware_test.go
@@ -0,0 +1,80 @@
+package blacklist
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/urfave/negroni"
+)
+
+func TestValidRequest(t *testing.T) {
+	called := false
+	mw := NewMiddleware().(*middleware)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+
+	a := negroni.New()
+	a.Use(mw)
+	a.Use(negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		called = true
+		next(w, r)
+	}))
+
+	a.ServeHTTP(response, req)
+	if !called {
+		t.Errorf("Expected middleware to call next")
+	}
+}
+
+func TestHealthcheckRequest(t *testing.T) {
+	notCalled := true
+	mw := NewMiddleware().(*middleware)
+
+	req := httptest.NewRequest("GET", "/healthcheck", nil)
+	response := httptest.NewRecorder()
+
+	a := negroni.New()
+	a.Use(mw)
+	a.Use(negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		notCalled = false
+		next(w, r)
+	}))
+
+	a.ServeHTTP(response, req)
+	if !notCalled {
+		t.Errorf("Expected middleware not to be called next")
+	}
+
+	if response.Code != http.StatusOK {
+		t.Errorf("Status should be OK for /healthcheck")
+	}
+}
+
+func TestBlacklistedRequest(t *testing.T) {
+	notCalled := true
+	mw := NewMiddleware().(*middleware)
+
+	for _, x := range blacklist {
+		req := httptest.NewRequest("GET", x, nil)
+		response := httptest.NewRecorder()
+
+		a := negroni.New()
+		a.Use(mw)
+		a.Use(negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+			notCalled = false
+			next(w, r)
+		}))
+
+		a.ServeHTTP(response, req)
+		if !notCalled {
+			t.Errorf("Expected middleware not to be called next")
+		}
+
+		if response.Code != http.StatusNotFound {
+			t.Errorf("Status should be Not Found for %v request", x)
+		}
+	}
+
+}
