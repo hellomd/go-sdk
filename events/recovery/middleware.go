0d7d5c09ad3221b14159603582c8994e996929dc
Gabriel Arrais <arraiscgabriel@gmail.com>
2017-10-18 19:05:26 -0200

    Fix recovery middleware implementation

--- events/recovery/middleware.go
+++ events/recovery/middleware.go
@@ -3,21 +3,51 @@ package recovery
 import (
 	"context"
 	"fmt"
+	"log"
 
+	raven "github.com/getsentry/raven-go"
 	"github.com/hellomd/go-sdk/events"
-	"github.com/sirupsen/logrus"
+	"github.com/hellomd/go-sdk/logger"
+	"github.com/hellomd/go-sdk/recovery/sentry"
 )
 
-func NewMiddleware() events.PipelineHandlerFunc {
-	return func(ctx context.Context, event *events.Event, next events.HandlerFunc) {
-		defer func() {
-			if r := recover(); r != nil {
-				err := fmt.Errorf("panic recovered: %v", r)
-				logrus.Error(err)
-				event.Reject(false, err)
-			}
-		}()
+type Recovery struct {
+	sentryDSN   string
+	handleError func(ctx context.Context, err error)
+}
 
-		next(ctx, event)
+func NewMiddleware(sentryDSN string) *Recovery {
+	return &Recovery{sentryDSN, handleError}
+}
+
+func (r *Recovery) Process(ctx context.Context, event *events.Event, next events.HandlerFunc) {
+	cli, err := raven.New(r.sentryDSN)
+	if err == nil {
+		ctx = sentry.SetInCtx(ctx, cli)
+	}
+
+	defer func() {
+		log.Println("lele")
+		if rMsg := recover(); rMsg != nil {
+			log.Println("lala")
+			err := fmt.Errorf("panic recovered: %v", rMsg)
+			event.Reject(false, err)
+			r.handleError(ctx, err)
+		}
+	}()
+
+	next(ctx, event)
+}
+
+// HandleError reports given error to logger and sentry when they are available
+func handleError(ctx context.Context, err error) {
+	logger, ctxErr := logger.GetFromCtx(ctx)
+	if ctxErr == nil {
+		logger.Error(err)
+	}
+
+	sentry, ctxErr := sentry.GetFromCtx(ctx)
+	if ctxErr == nil {
+		sentry.CaptureError(err, nil)
 	}
 }
