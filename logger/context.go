4926e2bfd69430302f118f7b3efbf14f4b2e09ee
Rodrigo Dumont <rlsdumont@gmail.com>
2017-10-18 16:41:06 -0200

    Create a no op logger

--- logger/context.go
+++ logger/context.go
@@ -3,13 +3,21 @@ package logger
 import (
 	"context"
 	"errors"
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 var errNotInCtx = errors.New("no logrus in context")
+var noOpLogger *logrus.Entry
 
 type ctxKey struct{}
+
+func init() {
+	l := logrus.New()
+	l.Out = ioutil.Discard
+	noOpLogger = l.WithFields(logrus.Fields{})
+}
 
 // GetFromCtx get the logrus instance from the context
 func GetFromCtx(ctx context.Context) (*logrus.Entry, error) {
@@ -23,4 +31,13 @@ func GetFromCtx(ctx context.Context) (*logrus.Entry, error) {
 // SetInCtx sets given logrus instance to given context
 func SetInCtx(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, ctxKey{}, logger)
+}
+
+func Get(ctx context.Context) *logrus.Entry {
+	logger, ok := ctx.Value(ctxKey{}).(*logrus.Entry)
+	if !ok {
+		return noOpLogger
+	}
+
+	return logger
 }
