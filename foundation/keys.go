d65883b2066cfcc7045d19773dd9405929c05bb5
Felix Ribeiro <sribeiro.felix@gmail.com>
2017-10-20 19:02:13 -0200

    Add foundation basics

---
+++ foundation/keys.go
@@ -0,0 +1,24 @@
+package foundation
+
+import (
+	"github.com/hellomd/go-sdk/config"
+)
+
+const (
+	// AppNameCfgKey -
+	AppNameCfgKey config.Key = "APP_NAME"
+	// PortCfgKey -
+	PortCfgKey config.Key = "PORT"
+	// EnvCfgKey -
+	EnvCfgKey config.Key = "ENV"
+	// LogmaticAPIKeyCfgKey -
+	LogmaticAPIKeyCfgKey config.Key = "LOGMATIC_API_KEY"
+	// SentryDSNCfgKey -
+	SentryDSNCfgKey config.Key = "SENTRY_DSN"
+	// SecretCfgKey
+	SecretCfgKey config.Key = "SECRET"
+	// AMQPURLCfgKey
+	AMQPURLCfgKey config.Key = "AMQP_URL"
+	// GatewayURLCfgKey
+	GatewayURLCfgKey config.Key = "GATEWAY_URL"
+)
